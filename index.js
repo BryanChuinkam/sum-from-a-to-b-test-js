147bf104b376008a034776cae5da9d802424133a
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-20 14:51:02 +0000

    wrote recursive function to sum all whole numbers between range

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+  //base case
+  if (fromN === toN) {
+    return toN;
+  }
+
+  //recursive case
+  return fromN + sum(fromN + 1, toN);
 }
+
+console.log(sum(3, 7)); // ->25
+
 
 module.exports = sum;
